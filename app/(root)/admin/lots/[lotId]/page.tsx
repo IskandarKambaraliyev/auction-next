33193d95275cbdac1685aa249343c00fdfd9e129
Iskandar Kambaraliyev <human.github@gmail.com>
2025-01-11 01:56:54 +0500

    changes

--- app/(root)/admin/lots/[lotId]/page.tsx
+++ app/(root)/admin/lots/[lotId]/page.tsx
@@ -2,6 +2,41 @@ import Create from "@/components/section/admin/Create";
 import getUserInfo from "@/hooks/getUserInfo";
 import prisma from "@/lib/db";
 
+const getLot = async (lotId: string) => {
+  return await prisma.auctionLot.findUnique({
+    where: {
+      id: lotId,
+    },
+    include: {
+      bids: {
+        orderBy: {
+          amount: "asc",
+        },
+      },
+      seller: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+      buyer: true,
+    },
+  });
+};
+
+export async function generateStaticParams() {
+  const lots = await prisma.auctionLot.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  return lots.map((lot) => ({
+    lotId: lot.id,
+  }));
+}
+
 type Props = {
   params: Promise<{ lotId: string }>;
 };
@@ -9,14 +44,7 @@ type Props = {
 export default async function AdminLotDetailPage({ params }: Props) {
   const { lotId } = await params;
 
-  const lot = await prisma.auctionLot.findUnique({
-    where: {
-      id: lotId,
-    },
-    include: {
-      bids: true,
-    },
-  });
+  const lot = await getLot(lotId);
 
   if (!lot) {
     return <p>Lot not found</p>;
